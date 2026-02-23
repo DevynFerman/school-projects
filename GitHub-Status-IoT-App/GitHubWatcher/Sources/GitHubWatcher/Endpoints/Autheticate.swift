9be58bdc8192c6429902eb3a9d3fbf2291375171
Devyn Ferman <devynlferman@gmail.com>
2026-02-23 16:16:43 -0500

    Temp commit to swap computers

--- GitHub-Status-IoT-App/GitHubWatcher/Sources/GitHubWatcher/Endpoints/Autheticate.swift
+++ GitHub-Status-IoT-App/GitHubWatcher/Sources/GitHubWatcher/Endpoints/Autheticate.swift
@@ -8,11 +8,6 @@
 import Foundation
 
 extension WatcherManager {
-    func isAuthenticated() throws -> Bool {
-        // TODO: Check Authorization Status
-        return true
-    }
-    
     func fetchGitHubUser(_ manager: WatcherManager) async throws {
         let url = URL(string: "https://api.github.com/users/\(manager.credentials.username)")!
         var request = URLRequest(url: url)
@@ -30,18 +25,7 @@ extension WatcherManager {
         print(json)
     }
     
-    func login() throws -> Bool {
-        // TODO: Login flow
-        return true
-    }
-    
-    func logout() throws -> Bool {
-        // TODO: Logout flow
-        return true
-    }
-    
-    func refreshToken() throws -> Bool {
-        // TODO: Refresh token flow
-        return true
+    func fetchRepos(_ manager: WatcherManager) async throws {
+        
     }
 }
